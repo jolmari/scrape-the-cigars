0d9160038cc8a077801b13891e0f6d345cee68bf
Jolmari <kristian.virmo@gmail.com>
2017-04-22 21:57:52 +0300

    Test server imitating target website structure

---
+++ ScraperTestServer/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ScraperTestServer.Controllers
+{
+    [Route("api")]
+    public class ValuesController : Controller
+    {
+        // GET api/5
+        [HttpGet("{index}")]
+        public ViewResult Get(int index)
+        {
+            Console.WriteLine(HttpContext.Connection.RemoteIpAddress);
+            return View("~/Templates/listing-page.cshtml", index);
+        }
+    }
+}
